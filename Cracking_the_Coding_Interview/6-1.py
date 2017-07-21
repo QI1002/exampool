0967d52d3e4c9cd63676dd00a9863a64f929d788
qi1002 <alanchang544@gmail.com>
2017-07-21 15:22:36 +0800

    add permutations for calculation

--- Cracking_the_Coding_Interview/6-1.py
+++ Cracking_the_Coding_Interview/6-1.py
@@ -1,15 +1,21 @@
+import sys
 import itertools 
 
 #TODO: from argv/argc
+#TODO: not use eval
+#TODO: use permutations
+
 op = [ '+' ,'-' ,'*' ,'/' ]
-item = [3 ,1 ,3 ,6]
+item = [3 ,1 ,3 ,6, 2]
 answer = 8 
 
 show_all = False
 op_radix = len(op)
 op_exp = len(item) - 1
 op_count = pow(op_radix, op_exp) 
-ass_count = len(list(itertools.permutations(range(3))))
+
+associations = list(itertools.permutations(range(op_exp)))
+ass_count = len(associations)
 
 print("count of operation and association is {0} {1}".format(op_count, ass_count))
 
@@ -21,22 +27,43 @@ def increase_index(index,radix,exp):
             index[i] = 0
     return index            
         
-indexList = []        
+indexList = []
+
 for i in range(op_exp):
     indexList.append(0)
- 
+     
 for i in range(op_count):
     if i != 0:
         indexList = increase_index(indexList, op_radix, op_exp)
-    eval_str = ""        
-    for j in range(len(item)):
-        if j != 0:
-            eval_str += str(op[indexList[j-1]])
-        eval_str += str(item[j])
-    result = eval(eval_str)
+
+    for j in range(ass_count):
+        assocList = [str(x) for x in item]
+        for k in range(op_exp):
+            kk = associations[j][k] 
+            eval_str = ""
+            eval_str += str(assocList[kk])
+            eval_str += str(op[indexList[kk]])
+            eval_str += str(assocList[kk+1])
+            eval_str = "(" + eval_str + ")"                        
+            for l in range(kk,0,-1):
+                if not ')' in assocList[l]:
+                    break
+                assocList[l] = eval_str
+            for l in range(kk+1,op_exp,1):
+                if not ')' in assocList[l]:
+                    break
+                assocList[l] = eval_str               
+            assocList[kk] = assocList[kk+1] = eval_str    
+            #print(str(k)+str(assocList))
+        #print(str(associations[j]) + " " +eval_str)
     
-    if (result == answer) or show_all:
-        print(eval_str + "={0}".format(eval(eval_str)))
+        try:
+            result = eval(eval_str)
+            if (result == answer) or show_all:
+                print(eval_str + "={0}".format(result))            
+        except:
+            print(eval_str + "={0}".format(sys.exc_info()[0]))
+            
         
             
     
