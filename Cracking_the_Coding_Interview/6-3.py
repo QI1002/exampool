8fbfc8594c0a32a82f536cec512de97a27885cdf
qi1002 <alanchang544@gmail.com>
2017-07-22 13:24:42 +0800

    refine the code to find the answer with steps display and save performance

--- Cracking_the_Coding_Interview/6-3.py
+++ Cracking_the_Coding_Interview/6-3.py
@@ -1,5 +1,7 @@
 import copy
   
+#reduce steps if go back
+#by argv and enlarge Jugs #  
 class Jug:
 
     def empty(self):
@@ -21,13 +23,13 @@ class Jug:
             source.volume = source.volume - (self.capacity - self.volume)
             self.volume = self.capacity
         else:
-            source.volume = 0
             self.volume += source.volume
+            source.volume = 0
 
 def print_results(results):
     print_str = "";
     for x in results:
-       print_str += "[" + str(x[0].volume) + "," + str(x[1].volume) + "]"
+       print_str += "[" + str(x[0]) + "," + str(x[1]) + "]"
     return print_str
                 
 def empty_a(a, b):
@@ -43,59 +45,71 @@ def full_b(a, b):
     b.full()
 
 def move_a2b(a,b):
-    a.setfrom(b)    
+    b.setfrom(a)    
     
 def move_b2a(a,b):
-    b.setfrom(a)    
+    a.setfrom(b)    
     
 def do_actions(id, a, b):
     if (id >= len(actions)):
         raise IndexError
     actions[id](a,b)
     
-a = Jug(5)
-b = Jug(3)        
-
 actions = [empty_a, empty_b, full_a, full_b, move_a2b, move_b2a]
 actions_desc = ["empty 5 quart jug", "empty 3 quart jug", "full 5 quart jug", "full 3 quart jug", "move 5 quart jug to 3 quart jug", "move 3 quart jug to 5 quart jug"]
 possible_result = []
 possible_flow = []
 answer = 4;
 
-result = [a,b]
+result = (0,0)
 flow = []
+state = 0
 
-values = set()
 results = []
 flows = []
+states = []
 
-values.add(result[0].volume)
-values.add(result[1].volume)
 results.append(result)
 flows.append(flow)
+states.append(state)
 
-maxloop = 4
+maxloop = 0
 loop = 0
     
 while(maxloop == 0 or loop < maxloop):
 
     for i in range(len(results)):
-        for j in range(len(actions)):
-            result = [copy.copy(results[i][0]), copy.copy(results[i][1])]
-            do_actions(j, result[0], result[1])
-            bool1 = result[0].volume in values
-            bool2 = result[1].volume in values
-            print("result = {0},{1}".format(result[0].volume, result[1].volume))
-            if (bool1 == False) or (bool2 == False):
+    
+        if states[i] != 0:
+            continue
+
+        for j in range(len(actions)):                        
+            jup5 = Jug(5, results[i][0])
+            jup3 = Jug(3, results[i][1])
+            flow = copy.copy(flows[i])
+            do_actions(j, jup5, jup3)
+            result = (jup5.volume, jup3.volume)
+            print("result = {0},{1}".format(result[0], result[1]))
+            if not result in results:
                 results.append(result)
-                if (bool1 == False): values.add(result[0].volume)
-                if (bool2 == False): values.add(result[1].volume)
-            
-            if (answer in values):
-                print("Found {0} by following steps:".format(answer))
+                flow.append(j)
+                flows.append(flow)
+                states.append(0)
+            else:
+                states[results.index(result)] = 1
+                    
+            if (answer == result[0] or answer == result[1]):
+                jup5 = Jug(5, 0)
+                jup3 = Jug(3, 0)           
+                print("Found {0} by following steps: {1}".format(answer, flow))
+                for k in range(len(flow)):             
+                    before = (jup5.volume, jup3.volume)
+                    do_actions(flow[k], jup5, jup3)
+                    after = (jup5.volume, jup3.volume)       
+                    print("step{0}: from {1} to {2} ({3})".format(k, before, after, actions_desc[flow[k]]))
                 exit(0)    
 
-        print("results = {0}".format(print_results(results)))        
+        print("results = {0}".format(results))        
 
     print("loop {0} =============".format(loop))        
     loop = loop + 1
