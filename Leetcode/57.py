80505d25e3b72b7d5e519d31b0b4487e9ad1ef74
QI1002 <alanchang544@gmail.com>
2017-09-13 21:32:56 +0800

    refine 57.py to be the better solution

--- Leetcode/57.py
+++ Leetcode/57.py
@@ -1,124 +1,51 @@
 
 #57. Insert Interval
 
-import copy
-def search(points, p):
-    l = 0
-    r = len(points)-1
-    while (r >= l):
-        m = (r+l)//2
-        #print((m,r,l))
-        if (points[m] == p): return m 
-        if (m == l):
-            if (points[l] > p): return l
-            else: return r if (points[r] > p) else r+1 
-        else:
-            if (points[m] < p):
-                l = m
-            else:
-                r = m
-
-def line_merge2(lines, target):
-    count = len(lines)
-    points = []
-    for i in range(count):    
-        points.append(lines[i][0])
-        points.append(lines[i][1])
-
-    s = search(points, target[0])
-    e = search(points, target[1])
-    result = []
-    extra = newline = newline2 = None
-
-    for i in range(count):
-        line = lines[i]
-
-        if (s == 2*i):    
-            newline = (target[0], line[1])         
-        else:
-            if (s == (2*i+1)):
-                newline = line
-        
-        if (e == 2*i):
-            if (lines[i][0] != target[1]):
-                newline2 = (newline[0], target[1])
-                extra = lines[i]
-            else:
-                newline2 = (newline[0], lines[i][1])
-        else:
-            if (e == (2*i+1)): 
-                newline2 = (newline[0], lines[i][1])          
-
-        if (s//2 > i or e//2 < i):
-            result.append(line)
-
-        if (newline2 != None):
-            result.append(newline2)
-            newline = newline2 = None
-
-        if (extra != None):
-            result.append(extra)
-            extra = None
-    
-    if (newline != None and newline2 == None):
-        newline2 = (newline[0], target[1]) 
-        result.append(newline2)
-     
-    if (s >= len(points)):
-        result = list(lines)
-        result.append(target)
-
-    return result
-
-def line_overlay2(lines, p, pp, ii):
-    if (pp//2 != ii): return False
-    even = ((pp & 0x1) == 0)
-    if (even and lines[pp//2][0] == p): return True
-    return (even == False)
-
-def line_overlay(lines, p, pp, ii):
-    return (lines[ii][0] <= p and lines[ii][1] >= p) 
+def line_overlay(line, p):
+    return (line[0] <= p and line[1] >= p) 
 
 def line_merge(lines, target):
-    count = len(lines)
-    points = []
-    for i in range(count):
-        points.append(lines[i][0])
-        points.append(lines[i][1])
 
-    s = search(points, target[0])
-    e = search(points, target[1])
     result = []
     newline = None
+    prev_end = target[0] if target[0] < lines[0][0] else lines[0][0]
+    prev_end -= 1
 
-    for i in range(count):
+    for i in range(len(lines)):
+
         overlay = False
         line = lines[i]
-        if (line_overlay(lines, target[0], s, i)):
+        prev_end = prev_end if i == 0 else lines[i-1][1]
+
+        if (line_overlay(line, target[0])):
             overlay = True
             newline = line
         else:
-           if (target[0] < line[0] and s == 2*i):
+           if (target[0] < line[0] and target[0] > prev_end):
                newline = (target[0], line[1])
+
         #print((newline, line, e))
-        if (line_overlay(lines, target[1], e, i)):
+
+        if (line_overlay(line, target[1])):
             newline = (newline[0], line[1])
             result.append(newline)
             overlay = True
             newline = None
         else:
-            if (target[1] < lines[0] and e == 2*i):
+            if (target[1] < line[0] and target[1] > prev_end):
                 newline = (newline[0], target[1])
                 result.append(newline)
                 newline = None
+
         #print((newline, overlay, result, line, s))
+
         if (newline == None and overlay == False):
             result.append(line)
 
     if (newline != None):
         result.append((newline[0], target[1]))
 
-    if (s >= len(points)):
+    if (target[0] > prev_end):
         result = list(lines)
         result.append(target)
 
@@ -126,14 +53,14 @@ def line_merge(lines, target):
                
 
 lines = [(2,3),(5,8),(11,13)]
-print(line_merge(copy.deepcopy(lines), (9,10)))
-print(line_merge(copy.deepcopy(lines), (9,11)))
-print(line_merge(copy.deepcopy(lines), (8,10)))
-print(line_merge(copy.deepcopy(lines), (8,11)))
-print(line_merge(copy.deepcopy(lines), (14,16)))
-print(line_merge(copy.deepcopy(lines), (0,1)))
-print(line_merge(copy.deepcopy(lines), (4,14)))
-print(line_merge(copy.deepcopy(lines), (0,10)))
-print(line_merge(copy.deepcopy(lines), (0,14)))
+print(line_merge(lines, (9,10)))
+print(line_merge(lines, (9,11)))
+print(line_merge(lines, (8,10)))
+print(line_merge(lines, (8,11)))
+print(line_merge(lines, (14,16)))
+print(line_merge(lines, (0,1)))
+print(line_merge(lines, (4,14)))
+print(line_merge(lines, (0,10)))
+print(line_merge(lines, (0,14)))
 
  
