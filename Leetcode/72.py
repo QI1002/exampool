bf2d3775c4dddf373776d48c9135f1f222e57bb7
QI1002 <alanchang544@gmail.com>
2017-09-20 20:46:27 +0800

    refine the solution of 72.py

--- Leetcode/72.py
+++ Leetcode/72.py
@@ -5,22 +5,25 @@ def editDistance(s1, s2):
     count1 = len(s1)
     count2 = len(s2)
     
-    row = [ 0 for i in range(count2) ]
-    matrix = [ list(row) for i in range(count1) ] 
-    row = [ [] for i in range(count2) ]
-    action = [ list(row) for i in range(count1) ] 
-    
-    for j in range(count2):
+    row = [ 0 for i in range(count2+1) ]
+    matrix = [ list(row) for i in range(count1+1) ] 
+    row = [ [] for i in range(count2+1) ]
+    action = [ list(row) for i in range(count1+1) ] 
+    #track = [ list(row) for i in range(count1+1) ]
+ 
+    for j in range(count2+1):
         matrix[0][j] = j
-        action[0][j] = [ (i,'n') for i in range(j) ]
-        
-    for i in range(1, count1, 1):
+        action[0][j] = [ (i,'insert') for i in range(j) ]
+         
+    for i in range(1, count1+1, 1):
         matrix[i][0] = i
-        action[i][0] = [ (j, 'm') for j in range(i) ]
-        for j in range(1, count2, 1):
-            if (s1[i] == s2[j]):
+        action[i][0] = [ (j, 'remove') for j in range(i) ]
+        for j in range(1, count2+1, 1):
+            if (s1[i-1] == s2[j-1]):
                 matrix[i][j] = matrix[i-1][j-1]
                 action[i][j] = action[i-1][j-1]
+                #track[i][j] = list(track[i-1][j-1])
+                #track[i][j].append((i-1,j-1))
             else:
                 r = matrix[i-1][j-1]+1
                 n = matrix[i][j-1]+1
@@ -29,18 +32,28 @@ def editDistance(s1, s2):
                 if (r < n):
                     matrix[i][j] = r
                     action[i][j] = list(action[i-1][j-1])
-                    action[i][j].append(((i,j), 'r'))                    
+                    action[i][j].append(((i-1,j-1), 'replace'))
+                    #track[i][j] = list(track[i-1][j-1])
+                    #track[i][j].append((i-1,j-1))
                 else:
                     matrix[i][j] = n 
                     action[i][j] = list(action[i][j-1])
-                    action[i][j].append((j, 'n'))
+                    action[i][j].append((j-1, 'insert'))
+                    #track[i][j] = list(track[i][j-1])
+                    #track[i][j].append((i,j-1))
                     
                 if (matrix[i][j] > m): 
                     matrix[i][j] = m
                     action[i][j] = list(action[i-1][j])
-                    action[i][j].append((i, 'm'))
-                                    
-    return matrix[count1-1][count2-1], action[count1-1][count2-1]
+                    action[i][j].append((i-1, 'remove'))
+                    #track[i][j] = list(track[i-1][j])
+                    #track[i][j].append((i-1,j))
+  
+    #track[count1][count2].append((count1, count2)) 
+    #print(track[count1][count2])                                
+    return matrix[count1][count2], action[count1][count2]
     
 print(editDistance("abcdefg", "acdefgh"))                
-                
+print(editDistance("abcdefg", "bcdefgh"))
+print(editDistance("abcdefg", "zabcdef"))
+print(editDistance("abcdefg", "azbcdef"))                
