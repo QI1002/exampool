bb3bf76ddfba67dd409b99dc3b793c6ec653297e
qi1002 <alanchang544@gmail.com>
2017-10-29 15:01:27 +0800

    add a all combination solution of 316.py but not good

--- Leetcode/301.py
+++ Leetcode/301.py
@@ -1,6 +1,7 @@
 
 #301 Remove Invalid Parentheses
 
+import copy
 def removeInvalidP(s):
     count = len(s)
     row = [ 0 for i in range(count) ] 
@@ -8,40 +9,58 @@ def removeInvalidP(s):
     
     for i in range(count):
         for j in range(count):
-            m[i][j] = []
+            m[i][j] = [[]]
             
     for i in range(count):
         for j in range(count-i):
             start = j
             end = j + i 
             if (i == 0):
-                m[start][end].extend([start])
+                m[start][end] = [[start]]
             else: 
                 if (i == 1):
                     if (s[start] != "(" or s[end] != ")"):
-                        m[start][end].extend([start, end])
+                        m[start][end] = [[start, end]]
                 else: 
                     if (s[start] == ")"):
-                        m[start][end].extend([start])    
-                        m[start][end].extend(m[start+1][end])
+                        m[start][end] = []
+                        for x in m[start+1][end]:
+                            y = list(x)
+                            y.insert(0, start)
+                            m[start][end].append(y)
                     else:
-                        m1, m2 = [], []
                         if (s[start+1] == ")"):
-                            m1 = list(m[start+2][end])
+                            m1 = copy.deepcopy(m[start+2][end])
                         else:
-                            m1 = [start]                            
-                            m1.extend(m[start+1][end])
+                            m1 = []
+                            for x in m[start+1][end]:
+                                y = list(x)
+                                y.insert(0, start)
+                                m1.append(y)
+
                         if (s[end] == ")"):
-                            m2 = list(m[start+1][end-1])
+                            m2 = copy.deepcopy(m[start+1][end-1])
                         else:
-                            m2 = list(m[start][end-1])
-                            m2.extend([end])
-                        
-                        m[start][end] = m2 if (len(m1) > len(m2)) else m1
+                            m2 = []
+                            for x in m[start][end-1]:
+                                y = list(x)
+                                y.append(end)
+                                m2.append(y)
+                                           
+                        if (len(m1[0]) == len(m2[0])):
+                            #print((start, end, m1, m2))
+                            m[start][end] = m1
+                            m[start][end].extend(m2)
+                        else:                        
+                            m[start][end] = copy.deepcopy(m2) if (len(m1[0]) > len(m2[0])) else copy.deepcopy(m1)
  
-    #print(m)                      
+    for i in range(count):
+        print(m[i])
+                               
     return m[0][count-1]
         
                         
 #print(removeInvalidP("(()"))
-print(removeInvalidP("()())()"))
+print(removeInvalidP("())()"))
+#print(removeInvalidP("()())()"))
+#print(removeInvalidP("()()())"))
