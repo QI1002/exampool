54f9039e0a410642f31384b4760fccf7438e741f
qi1002 <alanchang544@gmail.com>
2017-10-29 22:11:39 +0800

    add the fast solution for 315.py

--- Leetcode/315.py
+++ Leetcode/315.py
@@ -13,5 +13,55 @@ def countSmaller(data):
     
     return result
     
-sample = [5, 2, 6, 1]
+def countSmallerMerge(t, s, l, r, k):
+    for i in range(l, l+k, 1):
+        upper = len(t)-1
+        lower = 0
+        for j in range(l+k, r+1, 1):            
+            if (t[i] == t[j]): 
+                s[i] += s[j]
+                break
+
+            if (t[i] < t[j]):
+                uu = s[i]+s[j]
+                if (upper > uu): upper = uu  
+            else:
+                s[i] += 1
+                ll = s[i]+s[j]
+                if (lower < ll): lower = ll
+                   
+            if (upper == lower): 
+                s[i] = upper            
+                break
+            
+def countSmaller2(data):
+    count = len(data)
+    ss = [ 0 for i in range(count) ]
+    k = 1
+    while(k < count):
+        kk = k + k
+        for i in range(0, count, kk):
+            start = i
+            end = count if ((i+kk) > count) else i+kk
+            if ((end-start) > k): 
+                print((ss, start, end-1, k))
+                countSmallerMerge(data, ss, start, end-1, k)               
+        k = kk
+        
+    return ss    
+        
+sample = [5, 2, 6, 1, 9]
+print(countSmaller2(sample))
+print(countSmaller(sample))
+sample = [5, 2, 6, 1, 9, 4, 0]
+print(countSmaller2(sample))
+print(countSmaller(sample))
+sample = [1, 2, 3, 4, 5, 6, 7]
+print(countSmaller2(sample))
+print(countSmaller(sample))
+sample = [7, 6, 5, 4, 3, 2, 1]
+print(countSmaller2(sample))
+print(countSmaller(sample))
+sample = [7, 1, 1, 1, 1, 1, 1]
+print(countSmaller2(sample))
 print(countSmaller(sample))
