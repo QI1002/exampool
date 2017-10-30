923a3faa400a59874f51a5b32619afa8daabe413
QI1002 <alanchang544@gmail.com>
2017-10-30 12:45:30 +0800

    add the perfect solution of 330.py

--- Leetcode/330.py
+++ Leetcode/330.py
@@ -7,10 +7,10 @@ def patch(data, n):
     m = n
     tl = []
     
-    if (tm[0] == 1):
+    if (len(tm) > 0 and tm[0] == 1):
         tl.append(tm[0])
         tm.pop(0)
-        while(tm[0] <= (2* tl[-1])):
+        while(len(tm) > 0 and tm[0] <= (2* tl[-1])):
             tl.append(tm.pop(0))                 
     
     ss = sum(tl)
@@ -19,15 +19,29 @@ def patch(data, n):
         max = tm.pop()
         m -= max
      
-    print((m, ss, tl, tm)) 
-    while(m > 2):
-        h = m // 2
+    #print((m, ss, tl, tm)) 
+    while(m > ss):
+        h = (m+1) // 2
         r.append(h)
         m -= h
-    r.append(r)
-               
+
     return r
     
 nums, n = [1,3], 6
 print("{0}, {1} => {2}".format(nums, n, patch(nums, n)))            
-            
+
+nums, n = [1,5,10], 20
+print("{0}, {1} => {2}".format(nums, n, patch(nums, n)))            
+
+nums, n = [1,2,2], 5
+print("{0}, {1} => {2}".format(nums, n, patch(nums, n)))            
+
+ 
+for n in range(1,8,1):
+    nums = []
+    print("{0}, {1} => {2}".format(nums, n, patch(nums, n)))            
+    nums = [1]
+    print("{0}, {1} => {2}".format(nums, n, patch(nums, n)))            
+    nums = [1,2]
+    print("{0}, {1} => {2}".format(nums, n, patch(nums, n)))            
+           
