21084b723e91fdf8481152778b78432730ef8073
qi1002 <alanchang544@gmail.com>
2017-11-05 23:14:43 +0800

    add a total solution for 233.py to count 0 .. 9

--- Leetcode/233.py
+++ Leetcode/233.py
@@ -32,7 +32,47 @@ def count1(n):
         uu *= 10
 
     return count
+
+def countkExhaust(n, k):
+    count = 0
+    for i in range(1,n+1,1):
+        s = str(i)
+        for c in s:
+            if (ord(c) == (ord('0')+k)): count += 1
+
+    return count
+
+def countk(n, k):
+    digits = 0
+    bound = 1
+    while(bound <= n):
+        digits += 1        
+        bound *= 10    
     
-sample = 11
-print(count1Exhaust(sample))
-print(count1(sample))
+    count = 0
+    bound1 = 1
+    bound2 = 10
+    for i in range(digits):        
+        n1 = n // bound2   
+        n2 = (n - n1*bound2)//bound1
+        n3 = n - n1*bound2 - n2 * bound1
+                
+        count += n1*bound1
+        if (k == 0 and n1 > 0): count -= bound1
+                
+        if (n2 > k): 
+            if (not (k == 0 and n1 == 0)): count += bound1
+        else: 
+            if (n2 == k): count += (n3+1)
+
+        #print((i, n1, n2, n3, bound1, bound2, count))    
+        bound1 *= 10
+        bound2 *= 10
+        
+    return count
+
+for i in range(1000): #10000 ok also
+    for k in range(0, 10, 1):
+        c1 = countk(i, k)
+        c2 = countkExhaust(i, k)
+        if (c1 != c2): print((i, k, c1, c2))
