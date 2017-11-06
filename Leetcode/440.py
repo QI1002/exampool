1227af5ecf01aae34cd20b1d995340247e0befcc
QI1002 <alanchang544@gmail.com>
2017-11-06 14:35:38 +0800

    add the solution of 440.py

--- Leetcode/440.py
+++ Leetcode/440.py
@@ -25,36 +25,32 @@ def smallOrder(n, k):
     k -= 1
     lower = 0
     v = 0
-    while(True):
+    while(v <= n):
         for i in range(10):
             if (i == 0 and v == 0): continue
             r = numFirst(n, v+i)
-            #print((v,i,r,lower))
             upper = lower + sum(r)
+            #print((v,i,r,lower,upper))
             if (k >= lower and k < upper):
                 v += i
-                for j in range(len(r)):
-                    upper = lower + r[j]
-                    if (k == lower): return v
-                    if (k > lower and k < upper):
-                        if ((j+1) == len(r)): 
-                            return v + (k - lower)                    
-                        else:
-                            break
-                    lower = upper
-                    v *= 10
+                upper = lower + r[0]
+                if (upper == (lower+1) and k == lower): 
+                    return v
+                lower = upper
+                v *= 10
                 break
             lower = upper
         
     return None
 
-for i in range(1,15,1):
-    print(smallOrder(100,i))
-#r = []
-#for i in range(1, 101, 1):
-#    r.append(str(i))
-#    r = sorted(r)
-#    for j in range(i):
-#        c1 = smallOrder(i, j+1)
-#        c2 = int(r[j])
-#        if (c1 != c2): print((i, j, c1, c2))
+#for i in range(1,1001,1):
+#    print(smallOrder(1000,i))
+
+r = []
+for i in range(1, 1001, 1):
+    r.append(str(i))
+    r = sorted(r)
+    for j in range(i):
+        c1 = smallOrder(i, j+1)
+        c2 = int(r[j])
+        if (c1 != c2): print((i, j, c1, c2))
