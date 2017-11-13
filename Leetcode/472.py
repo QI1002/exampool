7a815d49b6eab7ee7104ea85f26fc7cc7a45aff4
qi1002 <alanchang544@gmail.com>
2017-11-13 08:21:43 +0800

    add the right solution of 472.py

--- Leetcode/472.py
+++ Leetcode/472.py
@@ -27,7 +27,43 @@ def concatWords(inputs):
         if (s not in result): result.append(s)
         
     return result
-     
-#samples = ["cat", "cats", "catsdogcats", "dog", "hippopotamuses", "ratdogcatrat"]        
+
 samples = ["a", "b", "c"]
 print(concatWords(samples))
+     
+class stack:
+    def __init__(self):
+        self.body = []
+
+    def push(self, data):
+        self.body.append(data)
+
+    def pop(self):
+        if (len(self.body) == 0):
+            return None
+        else:
+            return self.body.pop()
+
+    def isEmpty(self):
+        return True if len(self.body) == 0 else False
+     
+def findConcatWords(inputs, s):
+    ss = stack()
+    ss.push((s, []))
+    while(not ss.isEmpty()):
+        t = ss.pop()
+        if (len(t[0]) == 0):
+            if (len(t[1]) > 1): return t[1]
+            else: continue
+        for x in inputs:
+            if (len(t[0]) >= len(x) and t[0][:len(x)] == x):
+                y = list(t[1])
+                y.append(x)
+                ss.push((t[0][len(x):], y)) 
+                     
+    return None
+                     
+inputs = ["cat", "cats", "catsdogcats", "dog", "hippopotamuses", "rat", "ratdogcatrat"]        
+outputs = ["catsdogcats", "dogcatsdog", "ratcatdogcat"]
+for s in outputs:
+    print("{0}:{1}".format(s, findConcatWords(inputs, s)))
