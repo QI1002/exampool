117dcd49ad5b5067da1896e40db60687460dc9d5
qi1002 <alanchang544@gmail.com>
2017-11-18 21:34:17 +0800

    add the better solution of 446.py

--- Leetcode/446.py
+++ Leetcode/446.py
@@ -20,15 +20,39 @@ def allSlices(data):
                 ll.append(i)
         if (len(ll) > 2):
             lll = [ data[j] for j in ll ]
-            if (checkSlices(lll)):  result.append(lll)
+            if (checkSlices(lll) and not lll in result):  
+                result.append(lll)
 
-    return result
+    return sorted(result)
 
 def allSlices2(data):
-    pass
+
+    if (len(data) <= 2):
+        return []
+        
+    c = data[-1]    
+    t = allSlices2(data[:-1])
+    tt = [ list(x) for x in t ] 
+    for x in t:
+        if ((c-x[-1]) == (x[1]-x[0])):
+            xx = list(x)
+            xx.append(c)
+            if (not xx in tt): tt.append(xx)
+            
+    for j in range(len(data)-1,-1,-1):
+        cc = 2*data[j] - c 
+        if (cc in data[:j]):
+            yy = [cc, data[j], c]
+            if (not yy in tt): tt.append(yy)
+            
+    return sorted(tt)               
+            
 
 samples = [2,4,6,8,10]
 print(allSlices(samples))
 print(allSlices2(samples))
 
+samples = [7,7,7,7,7]
+print(allSlices(samples))
+print(allSlices2(samples))
 
