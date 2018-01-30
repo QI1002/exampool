d99db454cb8fcd707e6170762cc41daa82cc42e5
alanchang <alanchang544@gmail.com>
2018-01-30 17:06:17 +0800

    add the suggested solution of 11.py

--- Leetcode/11.py
+++ Leetcode/11.py
@@ -13,29 +13,30 @@ def myContainMost(data):
     while(len(ms) != 0):
         n = ms.pop()
         if ((n[0]*count) < maxarea): break
-        if (n[1] > max):
-            mm = (n[1]-min)*n[0]
-            max = n[1]
-            if (mm > maxarea): maxarea = mm
-            continue
-
-        if (n[1] < min):
-            mm = (max-n[1])*n[0]
-            min = n[1]
-            if (mm > maxarea): maxarea = mm
-            continue
 
         if ((n[1]-min) > (max-n[1])):
             mm = (n[1]-min)*n[0]
         else:
             mm = (max-n[1])*n[0]
             
-        if (mm > maxarea): mm = maxarea
+        if (mm > maxarea): maxarea = mm
+
+        if (n[1] > max): max = n[1]
+        if (n[1] < min): min = n[1]
 
     return maxarea
 
-def containMost(sample):
-    pass    
+def containMost(data):
+    l, r = 0, len(data)-1
+    maxarea = 0
+    while(l < r):
+        min = data[r] if data[l] > data[r] else data[l]
+        mm = (r-l)*min
+        if (mm > maxarea): maxarea = mm
+        if (data[l] > data[r]): r -= 1
+        else: l += 1
+
+    return maxarea
 
 sample = [1,8,6,2,5,4,8,3,7]
 print("{0}'s max area = {1},{2}".format(sample, myContainMost(sample), containMost(sample)))
