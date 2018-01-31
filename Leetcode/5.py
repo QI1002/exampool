145d3ebca5981dd1ea4e9979f697cd8b90a382ee
alanchang <alanchang544@gmail.com>
2018-01-31 15:36:56 +0800

    add the complete solution of 5.py

--- Leetcode/5.py
+++ Leetcode/5.py
@@ -10,6 +10,18 @@ def getLongest1(src, n):
         i += 1
     return src[n-i:n+i+1]
 
+def getLongest2(src, n):
+    count = len(src)
+    if ((n+1) >= count or src[n] != src[n+1]):
+        return "" 
+
+    i = 0
+    while((n-i-1) >= 0 and (n+i+2) < count):
+        if (src[n-i-1] != src[n+i+2]):
+            break
+        i += 1
+    return src[n-i:n+i+2]
+
 def longest(src):
     count = len(src)
     n = count//2
@@ -24,9 +36,11 @@ def longest(src):
         if (step > 0 and (2*n+1) <= len(maxt)):
             skip = True
         if (skip == False):    
-            t = getLongest1(src, n)
-            if (len(maxt) < len(t)): maxt = t
-            print(n, n1, n2, step, t)
+            t1 = getLongest1(src, n)
+            if (len(maxt) < len(t1)): maxt = t1
+            t2 = getLongest2(src, n)
+            if (len(maxt) < len(t2)): maxt = t2
+            print(n, n1, n2, step, t1, t2)
         if (step > 0): 
             n = n2 + step
             n2 = n
@@ -34,8 +48,11 @@ def longest(src):
             n = n1 + step 
             n1 = n
         step *= -1
-    
+   
     return maxt
     
 s = "babadd"    
 print("{0} => {1}".format(s, longest(s)))
+s = "baabdd"
+print("{0} => {1}".format(s, longest(s)))
+
