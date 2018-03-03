c6bbb353b634604e4e6c0355799ce1f432c5a862
alanchang <alanchang544@gmail.com>
2018-03-03 11:00:36 +0800

    refine the solution of 296.py

--- Leetcode/296.py
+++ Leetcode/296.py
@@ -11,6 +11,13 @@ def meeting2D(d2):
     dy = [ y for x,y in d2 ]
     return (meeting1D(dx), meeting1D(dy))
 
+def shortDist(d2):
+    s = meeting2D(d2)
+    d = 0
+    for home in d2:
+        d += abs(home[0]-s[0])
+        d += abs(home[1]-s[1])
+    return (s,d)
 
 given = [(0,0),(0,4),(2,2)]
-print("{0}:{1}".format(given, meeting2D(given)))
+print("{0}:{1}".format(given, shortDist(given)))
