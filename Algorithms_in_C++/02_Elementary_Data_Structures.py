b85bb461c37e684ba59f9a8b65df3c95146b1433
alanchang <alanchang544@gmail.com>
2018-03-08 20:46:00 +0800

    add a better solution of 02_Elementary_Data_Structures.py

--- Algorithms_in_C++/02_Elementary_Data_Structures.py
+++ Algorithms_in_C++/02_Elementary_Data_Structures.py
@@ -1,22 +1,47 @@
 
 
 def intopost(s):
+    #print(s)
     stack = []
     result = []
-    x = ""
+    ll = x = ""
+    l = 0     
     for c in s:
+        if (l > 0): 
+            ll += c 
+            if (c != '(' and c != ')'): continue
+
         if ord(c) >= ord('0') and ord(c) <= ord('9'): x += c
         else: 
-            if (x != ""): result.append(x) 
+            if (x != ""): 
+                result.append(x) 
+                if (len(stack) > 0 and stack[-1] == '*'): 
+                    result.append(stack.pop())
             x = ""
-        if c == ')': result.append(stack.pop())
+
+        if c == '(': l += 1
+        if c == ')': 
+            l -= 1 
+            if (l == 0):
+                result.extend(intopost(ll[:-1]))
+                ll = ""
+
         if c == '+' or c == '-': stack.append(c)
         if c == '*' : stack.append(c)
 
-    if x != "": result.append(x)
-    while(len(stack)): result.append(stack.pop())
+    if x != "": 
+        result.append(x)
+        if (len(stack) > 0 and stack[-1] == '*'): 
+            result.append(stack.pop())
+
+    while(len(stack) != 0): result.append(stack.pop())
+
     #print(result)
-    
+    return result
+
+def calc(s):
+   
+    result = intopost(s) 
     stack = []
     while(len(result) != 0):
         c = result.pop(0)
@@ -27,10 +52,18 @@ def intopost(s):
 
     return stack.pop(0)
 
+expr = "(5*2)+3"
+print("{0}={1}".format(expr, calc(expr)))
+expr = "5*2+3"
+print("{0}={1}".format(expr, calc(expr)))
+expr = "(5+2)*3"
+print("{0}={1}".format(expr, calc(expr)))
 expr = "5+2*3"
-print("{0}={1}".format(expr, intopost(expr)))
+print("{0}={1}".format(expr, calc(expr)))
 expr = "5*(((9-8)*(4*6))+7)"
-print("{0}={1}".format(expr, intopost(expr)))
+print("{0}={1}".format(expr, calc(expr)))
 expr = "5*(((19-18)*(4*26))+37)"
-print("{0}={1}".format(expr, intopost(expr)))
+print("{0}={1}".format(expr, calc(expr)))
+expr = "(5*(((19-18)*(4*26))+37))"
+print("{0}={1}".format(expr, calc(expr)))
 
