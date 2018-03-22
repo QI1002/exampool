815e9501bbf8ff19c2ec9ab7375e3e37a3d9bacf
alanchang <alanchang544@gmail.com>
2018-03-22 18:55:14 +0800

    add the defect contex2 and refine contex for chapter25.py

--- Algorithms_in_C++/25_Find_the_Convex_Hull.cpp
+++ Algorithms_in_C++/25_Find_the_Convex_Hull.cpp
@@ -18,13 +18,17 @@ typedef struct _point2
     float f;
 }point2;
 
-class ycomp
+class fcomp
 {
 public:
-    bool operator ()(const point2 &p1, const point2 &p2)
+    fcomp(vector<point2> ps) { this->ps = ps; }	
+    bool operator ()(const int a, const int b)
     {
-        return p1.p.y > p2.p.y;        
+        return ps[a].f < ps[b].f;
     }
+
+private:
+    vector<point2> ps;
 };
 
 // (0,0) => (1,  0): 0 
@@ -50,10 +54,24 @@ float theta(point &p1, point &p2)
    return t;
 }
 
-void findConvex(point ps[], int num)
+int CCW(point &p0, point &p1, point &p2)
+{
+    int dx1, dx2, dy1, dy2;
+    
+    dx1 = p1.x - p0.x; dy1 = p1.y - p0.y;
+    dx2 = p2.x - p0.x; dy2 = p2.y - p0.y;
+
+    if (dx1*dy2 > dy1*dx2) return 1;
+    if (dx1*dy2 < dy1*dx2) return -1;
+    // if equal means dx1/dy1 == dx2/dy2
+    if ((dx1*dx2 < 0) || (dy1*dy2 < 0)) return -1;
+    if ((dx1*dx1+dy1*dy1) < (dx2*dx2+dy2*dy2)) return 1;
+    return 0;
+}
+
+void findConvex2(point ps[], int num, vector<int> &result)
 {
     vector<point2> vp(num);
-    vector<int> result;
     int first = -1;
     for(int i = 0; i< num; i++)
     {
@@ -64,6 +82,47 @@ void findConvex(point ps[], int num)
     }
 
     cout << first << endl;
+    vector<int> order(num);
+    for(int i = 0; i < vp.size(); i++)
+    {
+        vp[i].f = theta(vp[first].p, vp[i].p);
+        order[i] = i;
+    }
+    
+    sort(order.begin(), order.end(), fcomp(vp));
+    for(int i = 0; i < order.size(); i++)
+        cout << order[i] << ",";
+    cout << endl;
+    
+    int s1 = order[0];
+    int s2 = order[1];
+    int s3 = order[2];
+    result.emplace_back(s1); 
+    int n = order.size();
+    order.emplace_back(s1);
+    for(int i = 1; i < n; i++)
+    {
+	s1 = result[result.size()-1]; 
+	s2 = order[i];
+	s3 = order[i+1];
+        int ccw = CCW(vp[s1].p, vp[s2].p, vp[s3].p);
+	if (ccw == 1) result.emplace_back(s2);
+    }
+}
+
+void findConvex(point ps[], int num, vector<int> &result)
+{
+    vector<point2> vp(num);
+    int first = -1;
+    for(int i = 0; i< num; i++)
+    {
+	if (first == -1 || ps[first].y < ps[i].y) first = i;    
+        vp[i].p = ps[i];
+	vp[i].i = i;
+	vp[i].f = 0;
+    }
+
+    //cout << first << endl;
     //sort(vp.begin(), vp.end(), ycomp());
     float prevf = 0.0;
     int next = first;
@@ -76,32 +135,37 @@ void findConvex(point ps[], int num)
 	{   
 	    if (i == next) continue; 
             float f = theta(vp[next].p, vp[i].p);
-	    //cout << i << " " << f << endl;
 	    if (f < prevf) continue;
 	    if (next2 == -1 || minf > f) { minf = f; next2 = i; }
 	}
       
 	if (next2 == -1) { prevf = 0; continue; }
-	//bool found = false;
-	//for(int j=0; j < result.size(); j++) if (result[j] == next2) found = true;  
-        //if (found) break;
+        if (next2 == first) break;
 
 	next = next2;
 	prevf = minf;
-	cout << next << "=========" << minf << endl;
 	result.emplace_back(next);
-    }while(next != first);
 
-    //for(int i = 0; i < result.size(); i++)
-    //    cout << result[i] << ",";
-    //cout << endl;
+    }while(true);
+
 }
 
 int main(int argc, char* argv[])
 {
     point ps[] = {{3,9},{11,1},{6,8},{4,3},{5,15},{8,11},{1,6},{7,4},
 	          {9,7},{14,5},{10,13},{16,14},{15,2},{13,16},{3,12},{12,10}};
+    
+    vector<int> result;
+    findConvex(ps, ELEMOF(ps), result);
+    for(int i = 0; i < result.size(); i++)
+        cout << result[i] << ",";
+    cout << endl;
 
-    findConvex(ps, ELEMOF(ps));
+    vector<int> result2;
+    findConvex2(ps, ELEMOF(ps), result2);
+    for(int i = 0; i < result2.size(); i++)
+        cout << result2[i] << ",";
+    cout << endl;
+    
     return 0;
 }
