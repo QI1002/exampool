ff84f2c8310edb66c8a0909314936e4364c41645
alanchang <alanchang544@gmail.com>
2018-03-25 23:58:32 +0800

    add the solution to test polygon inside for chapter25.cpp

--- Algorithms_in_C++/25_Find_the_Convex_Hull.cpp
+++ Algorithms_in_C++/25_Find_the_Convex_Hull.cpp
@@ -18,6 +18,11 @@ typedef struct _point2
     float f;
 }point2;
 
+typedef struct _line 
+{
+    point s,e;
+}line;
+
 class fcomp
 {
 public:
@@ -60,7 +65,7 @@ int CCW(point &p0, point &p1, point &p2)
     
     dx1 = p1.x - p0.x; dy1 = p1.y - p0.y;
     dx2 = p2.x - p0.x; dy2 = p2.y - p0.y;
-
+    //cout << dx1 << " " << dx2 << " " << dy1 << " " << dy2 << endl;
     if (dx1*dy2 > dy1*dx2) return 1;
     if (dx1*dy2 < dy1*dx2) return -1;
     // if equal means dx1/dy1 == dx2/dy2
@@ -68,6 +73,94 @@ int CCW(point &p0, point &p1, point &p2)
     if ((dx1*dx1+dy1*dy1) < (dx2*dx2+dy2*dy2)) return 1;
     return 0;
 }
+
+// not ok when test ((0,6)(100,6)) and ((1,6)(1,6))
+bool isIntersect(line l1, line l2)
+{
+    int ccw1 = CCW(l1.s, l1.e, l2.s);
+    int ccw2 = CCW(l1.s, l1.e, l2.e);
+    int ccw3 = CCW(l2.s, l2.e, l1.s);
+    int ccw4 = CCW(l2.s, l2.e, l1.e);
+
+    //cout << ccw1 << " " << ccw2 << " " << ccw3 << " " << ccw4 << endl;
+    return (ccw1*ccw2 <= 0 && ccw3*ccw4 <= 0);
+}
+
+bool inLine(point p, line l)
+{
+    int dx1 = p.x - l.s.x;
+    int dx2 = p.x - l.e.x;
+    int dy1 = p.y - l.s.y;
+    int dy2 = p.y - l.e.y;
+    if ((dx1*dx2) <= 0 && (dy1*dy2) <= 0)
+    {
+        if ((dx1*dy2) == (dx2*dy1))
+	{
+	    //cout << p.x << " " << p.y << " " << l.s.x << " " << l.s.y << " " << l.e.x << " " << l.e.y << endl;	
+	    return true;
+	}
+    }
+
+    return false;		    
+}
+
+bool isInside(vector<point> &bound, point test, int xmax)
+{
+    line testl = { test, test };
+    
+    testl.e.x = 100; //xmax; // not use INT_MAX but 100 for not overflow
+    int count = 0;
+    
+    for(int i = 1; i <= bound.size(); i++)
+    {
+	line edge;
+
+	edge.e = (i == bound.size()) ? bound[0] : bound[i];
+	edge.s = bound[i-1];
+        if (inLine(edge.e, testl)) continue;  
+        if (isIntersect(testl, edge)) count++; 
+    }
+
+    return (count & 1);
+}
+
+void testInside(point ps[], int num, vector<int> &convex)
+{
+    vector<point> bound;	
+    vector<bool> inConvex(num, false);
+    for(int i = 0; i < convex.size(); i++)
+    {
+	int index = convex[i];
+	inConvex[index] = true;
+        bound.emplace_back(ps[index]);
+    }
+
+
+    point left, right, top, bottom;
+    right = left = bound[0];
+    bottom = top = bound[0];
+    for(int i = 1; i < bound.size(); i++)
+    {
+        if (left.x > bound[i].x) left = bound[i];
+        if (right.x < bound[i].x) right = bound[i];
+        if (top.y > bound[i].y) top = bound[i];
+        if (bottom.y < bound[i].y) bottom = bound[i];
+    }
+    
+    left.x--; right.x++; top.y--; bottom.y++;	    
+    
+    for(int i = 0; i < num; i++)
+    {
+        if (inConvex[i]) continue;
+	cout << i << " inside = " << isInside(bound, ps[i], right.x) << endl;
+    }
+
+    cout << "left inside = " << isInside(bound, left, right.x) << endl; 
+    cout << "right inside = " << isInside(bound, right, right.x) << endl; 
+    cout << "top inside = " << isInside(bound, top, right.x) << endl; 
+    cout << "bottom inside = " << isInside(bound, bottom, right.x) << endl; 
+}
+
 
 void findConvex2(point ps[], int num, vector<int> &result)
 {
@@ -158,7 +251,6 @@ void findConvex(point ps[], int num, vector<int> &result)
 	result.emplace_back(next);
 
     }while(true);
-
 }
 
 int main(int argc, char* argv[])
@@ -177,6 +269,12 @@ int main(int argc, char* argv[])
     for(int i = 0; i < result2.size(); i++)
         cout << result2[i] << ",";
     cout << endl;
-    
+   
+    cout << "====================" << endl;
+    //line l1 = { ps[6], ps[6] };
+    //l1.s.x = 0; l1.e.x = 100;
+    //line l2 = { ps[6], ps[6] };
+    //cout << inLine(ps[6], l2) << endl;
+    testInside(ps, ELEMOF(ps), result);
     return 0;
 }
