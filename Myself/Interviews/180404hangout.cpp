9803d6f8ce49c701a55d7b8adfbc88fb7d7404f1
alanchang <alanchang544@gmail.com>
2018-04-11 15:02:07 +0800

    refine the solution of 180404 hangout interview

--- Myself/Interviews/180404hangout.cpp
+++ Myself/Interviews/180404hangout.cpp
@@ -7,6 +7,7 @@
 //avg = 1/(26^n) * (1*(1^n-0^n)+2*(2^n-1^n)+... 26*(26^n-(25)^n))
 
 #include <vector>
+#include <set>
 #include <string>
 #include <iostream>
 #include <math.h>
@@ -31,42 +32,32 @@ void scoreWithPuzzle(string &p, string &g, string &a)
     for(; i < p.size(); i++) a += '-';
 }
 
-bool game(string &puzzle, string &result)
+int game(string &puzzle, string &result)
 {
-    vector<vector<int>> check;
     const int count = 26;
-    
-    vector<int> vi(count, -1);
-    for(int i = 0; i < puzzle.size(); i++) 
-        check.emplace_back(vi);
-    
+    int calls = 0;
     string guess;
     result = "";
     int match;
     for(int i = 0; i < puzzle.size(); i++) result += "-";
+    
     do
     { 
         guess = result;
         for(int i = 0; i < puzzle.size(); i++)
         {
-            int j;
 	    if (result[i] != '-') continue; 
-            for(j = 0; j < count; j++) 
-                if (check[i][j] == -1) break;
-            if (j >= count) 
-            { cout << "can’t guess this puzzle" << endl; return false; }
-            guess[i] = (char)('a'+j);  
+            if (calls >= count) 
+            { cout << "can’t guess this puzzle" << endl; return -1; }
+            guess[i] = (char)('a'+ calls);  
         }
 
-        scoreWithPuzzle(puzzle, guess, result);
+        scoreWithPuzzle(puzzle, guess, result); calls++;
         match = 0;
         for(int i = 0; i < puzzle.size(); i++) 
-        {    
-            if (result[i] == '-') 
-            { check[i][guess[i]-'a'] = 0; continue; }
-            match++; 
-            check[i][guess[i]-'a'] = 1;
-        } 
+            if (result[i] != '-') match++;
+
+	cout << calls << "(" << match << "):" << result << endl;
     }while(match != puzzle.size());
 
     return true;
@@ -82,8 +73,8 @@ int main(int argc, char* argv[])
     for(int i = 0; i < n; i++) puzzle[i] = (char)('a'+(rand()%count)); 
 
     string result;
-    bool r = game(puzzle, result);
-    if (r == true) 
+    int r = game(puzzle, result);
+    if (r > 0) 
     {
         if (result == puzzle) cout << "guess " << puzzle << " right" << endl;
         else cout << "guess " << puzzle << " wrong" << endl;
